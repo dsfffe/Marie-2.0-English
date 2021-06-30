8738825c91efd58f6b1b7266f8c1f1e8e2d90b0d
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-06-30 23:06:55 +0200

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,23 +19,16 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
-
 im a group manager bot maintained by  [this person](tg://user?id={}).
-
-My future updates will be put into This Channel - @sylix_team & My Support Group @sylixgroup.
-
-
-
+My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
+you can create clone same like me..
 For more commands click /help...
-
-**hello world**
-
+**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 """
 
 HELP_STRINGS = """
-
 Hello! my name *{}*.
-
 *Main* available commands:
  - /start: Start the bot...
  - /help: help....
@@ -43,7 +36,6 @@ Hello! my name *{}*.
  - /settings:
    - in PM: To find out what SETTINGS you have set....
    - in a group:
-
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
@@ -52,7 +44,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/sylix)"""
+There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
